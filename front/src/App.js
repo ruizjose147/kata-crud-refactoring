316bd83bf313edd35c680d2d3c9d8b3ad4181ab3
JoseRuiz <ruizjose_147@hotmail.com>
2022-02-23 18:52:50 -0500

    Refactorizando coṕonentes en el front

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,6 @@
 import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
+import Form from './componets/Form';
+import List from './componets/List';
 
 const HOST_API = "http://localhost:8080/api";
 const initialState = {
@@ -6,79 +8,7 @@ const initialState = {
 };
 const Store = createContext(initialState)
 
-
-const Form = () => {
-  const formRef = useRef(null);
-  const { dispatch, state: { todo } } = useContext(Store);
-  const item = todo.item;
-  const [state, setState] = useState(item);
-
-  const onAdd = (event) => {
-    event.preventDefault();
-
-    const request = {
-      name: state.name,
-      id: null,
-      completed: false
-    };
-
-
-    fetch(HOST_API + "/todo", {
-      method: "POST",
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then((todo) => {
-        dispatch({ type: "add-item", item: todo });
-        setState({ name: "" });
-        formRef.current.reset();
-      });
-  }
-
-  const onEdit = (event) => {
-    event.preventDefault();
-
-    const request = {
-      name: state.name,
-      id: item.id,
-      isCompleted: item.isCompleted
-    };
-
-
-    fetch(HOST_API + "/todo", {
-      method: "PUT",
-      body: JSON.stringify(request),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then((todo) => {
-        dispatch({ type: "update-item", item: todo });
-        setState({ name: "" });
-        formRef.current.reset();
-      });
-  }
-
-  return <form ref={formRef}>
-    <input
-      type="text"
-      name="name"
-      placeholder="¿Qué piensas hacer hoy?"
-      defaultValue={item.name}
-      onChange={(event) => {
-        setState({ ...state, name: event.target.value })
-      }}  ></input>
-    {item.id && <button onClick={onEdit}>Actualizar</button>}
-    {!item.id && <button onClick={onAdd}>Crear</button>}
-  </form>
-}
-
-
-const List = () => {
+/*const List = () => {
   const { dispatch, state: { todo } } = useContext(Store);
   const currentList = todo.list;
 
@@ -147,7 +77,7 @@ const List = () => {
       </tbody>
     </table>
   </div>
-}
+}*/
 
 
 
